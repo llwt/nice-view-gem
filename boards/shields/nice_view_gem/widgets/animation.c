c250e3273630040ff0c71e02c178a2e297117113
Steven Nance <stevenmnance@gmail.com>
2026-03-20 13:28:39 +0100

    feat: add config toggle between crystal and hammerbeam art
    Add NICE_VIEW_GEM_ART_CRYSTAL / NICE_VIEW_GEM_ART_SLIDESHOW Kconfig
    choice to select peripheral screen artwork. Slideshow is the default.
    Animation timing defaults adjust per art style (960ms for crystal,
    300000ms for slideshow).

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -2,6 +2,36 @@
 #include <zephyr/kernel.h>
 #include "animation.h"
 
+#if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ART_CRYSTAL)
+
+LV_IMG_DECLARE(crystal_01);
+LV_IMG_DECLARE(crystal_02);
+LV_IMG_DECLARE(crystal_03);
+LV_IMG_DECLARE(crystal_04);
+LV_IMG_DECLARE(crystal_05);
+LV_IMG_DECLARE(crystal_06);
+LV_IMG_DECLARE(crystal_07);
+LV_IMG_DECLARE(crystal_08);
+LV_IMG_DECLARE(crystal_09);
+LV_IMG_DECLARE(crystal_10);
+LV_IMG_DECLARE(crystal_11);
+LV_IMG_DECLARE(crystal_12);
+LV_IMG_DECLARE(crystal_13);
+LV_IMG_DECLARE(crystal_14);
+LV_IMG_DECLARE(crystal_15);
+LV_IMG_DECLARE(crystal_16);
+
+const lv_img_dsc_t *anim_imgs[] = {
+    &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
+    &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
+    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+};
+
+#define ANIM_IMG_COUNT 16
+#define ANIM_X_OFFSET 36
+
+#else /* CONFIG_NICE_VIEW_GEM_ART_SLIDESHOW */
+
 LV_IMG_DECLARE(hammerbeam1);
 LV_IMG_DECLARE(hammerbeam2);
 LV_IMG_DECLARE(hammerbeam3);
@@ -42,12 +72,17 @@ const lv_img_dsc_t *anim_imgs[] = {
     &hammerbeam26, &hammerbeam27, &hammerbeam28, &hammerbeam29, &hammerbeam30,
 };
 
+#define ANIM_IMG_COUNT 30
+#define ANIM_X_OFFSET 0
+
+#endif
+
 void draw_animation(lv_obj_t *canvas) {
 #if IS_ENABLED(CONFIG_NICE_VIEW_GEM_ANIMATION)
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 30);
+    lv_animimg_set_src(art, (const void **)anim_imgs, ANIM_IMG_COUNT);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
@@ -61,5 +96,5 @@ void draw_animation(lv_obj_t *canvas) {
     lv_img_set_src(art, anim_imgs[random_index]);
 #endif
 
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, ANIM_X_OFFSET, 0);
 }
